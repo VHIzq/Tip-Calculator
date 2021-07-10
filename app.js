0b3d7204de55f5d11f502f23267a6578e0ecf83e
Victor Izquierdo <85770256+io31416@users.noreply.github.com>
2021-07-09 20:53:51 -0500

    Finished

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-//Creación de la app
+//Creating the app
 let app = () => {
 let bill = document.getElementById("bill-count").value;//Input the bill amount
 let rate = document.getElementById("rate").value; // Input the quality of service
 let Numpeople = document.getElementById("people").value; //Input the number of diners
-//Calculate the tip depending the service experience
+//Validate input
+if(bill == "" || rate == 0){
+  alert("Please, enter values");
+  return;
+}
+//Calculating the tip depending the service experience
 let tip = (bill * rate ) / Numpeople;
-tip = Math.round(tip * 100) / 100;
-tip = tip.toFixed(2);
+tip = Math.round(tip * 100) / 100; //round to two decimal places
+tip = tip.toFixed(2); // With two digits after decimal point
 document.getElementById("tip").innerHTML = tip;
 }
 document.getElementById("calculate").onclick = function(){
